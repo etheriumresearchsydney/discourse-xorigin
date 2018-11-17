b235228af5982b61860c506837795fef41a83178
Virgil Griffith <i@virgil.gr>
2018-11-17 13:56:20 +0800

    added notes.ethereum.org

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,11 @@
 # name: Set X-Frame-Options correctly
 # about: Set site headers
-# version: 1
-# authors: Julian
+# version: 1.1
+# authors: Julian and Virgil Griffith
 
-Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
-Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Origin' => '*'})
+  
+Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'allow-from https://notes.ethereum.org'})
+Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Origin' => 'https://notes.ethereum.org'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS, DELETE'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'})
+
