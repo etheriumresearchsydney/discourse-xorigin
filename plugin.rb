ea0dba96c2733615f9894a46791cd43401d0f038
Hsiao-Wei Wang <hwwang156@gmail.com>
2020-01-16 00:37:34 +0800

    Add url and name

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,10 @@
-# name: Set X-Frame-Options correctly
+# name: discourse-xorigin-hackmd
 # about: Set site headers
-# version: 1.1
+# version: 1.2
 # authors: Julian and Virgil Griffith
-
+# url: https://github.com/ethresearch/discourse-xorigin
   
 Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'allow-from https://notes.ethereum.org'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Origin' => 'https://notes.ethereum.org'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS, DELETE'})
 Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'})
-
